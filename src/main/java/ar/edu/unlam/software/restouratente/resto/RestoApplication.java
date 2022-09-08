1083854f9ab445c0f09d3882b806967649fe4b4f
Nico-Larsen <nicolas@camonapp.com>
2022-09-08 00:07:46 -0300

    First commit

---
+++ src/main/java/ar/edu/unlam/software/restouratente/resto/RestoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.software.restouratente.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlam/software/restouratente/resto/RestoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.software.restouratente.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
